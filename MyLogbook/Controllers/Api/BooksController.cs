76831ca1bf545ae4fdbd5746a296d973f6f8c85a
Christophe Ayel <CAYEL@cegid.fr>
2017-06-22 12:57:38 +0200

    webapi books : sample

---
+++ MyLogbook/Controllers/Api/BooksController.cs
@@ -0,0 +1,46 @@
+﻿using MyLogbook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyLogbook.Controllers.Api
+{
+    public class BooksController : ApiController
+    {
+        // GET: api/Books
+        public IEnumerable<Book> Get()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            IEnumerable<Book> books = new List<Book>();
+            books = context.Books;
+            return books.ToList();
+        }
+
+        // GET: api/Books/5
+        /*
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Books
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Books/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Books/5
+        public void Delete(int id)
+        {
+        }
+         */
+ 
+    }
+}
